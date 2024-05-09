0265de71d7458ae1345656de9ed9057e646d1a5c
Jin <zinkoko5211@gmail.com>
2024-05-09 20:40:37 +0900

    first commit

---
+++ app/models/tweet.rb
@@ -0,0 +1,12 @@
+class Tweet < ApplicationRecord
+
+  def attributes
+    {'text' => nil, 'image' => nil}
+  end
+
+
+  validates :text, presence: true
+  belongs_to :user
+  has_many :comments
+
+end
